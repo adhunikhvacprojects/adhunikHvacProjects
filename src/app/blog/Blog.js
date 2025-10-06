3a382942c447300c896f28f48c9052c947d81384
adhunikhvacprojects <info@adhunikhvacprojects.com>
2025-10-06 11:39:42 +0530

    Update Blog.js
    blog page fixing error

--- src/app/blog/Blog.js
+++ src/app/blog/Blog.js
@@ -16,15 +16,7 @@ export default function Blog() {
       img: "https://images.unsplash.com/photo-1648587096714-170302c4c922?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
 
-    {
-      id: 2,
-      title: "Top HVAC Contractor in Delhi NCR | Adhunik HVAC Projects",
-
-      date: "September 2025",
-
-      img: "https://images.unsplash.com/photo-1648587096714-170302c4c922?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      link: "",
-    },
+ 
     
   ];
 
